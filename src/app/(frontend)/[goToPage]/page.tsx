c04aaac6f000652c008c4a4fc0186516d8fea35e
Silje Skogstrand Moe <siljeskogstrand@gmail.com>
2025-12-03 09:26:02 +0100

    fix: typeguard for bilder på bokoversikten

--- src/app/(frontend)/[goToPage]/page.tsx
+++ src/app/(frontend)/[goToPage]/page.tsx
@@ -4,9 +4,38 @@ import { getPayload } from 'payload'
 import Link from 'next/link'
 import BookCard from '@/components/BookCard/BookCard'
 import Button from '@/components/Button/Button'
+import type { Bookcover } from '@/payload-types'
 
 type BooksPageProps = {
   params: Promise<{ goToPage: string }>
+}
+
+type GuardTypes = {
+  sizes: {
+    thumbnail: {
+      url: string
+      width: number
+      height: number
+    }
+  }
+}
+
+//Framgangsmåte hentet fra MinGA https://lms.gokstadakademiet.no/course/view.php?id=349#module-16132
+//Under Bildebehandling med Payload og Sharp/Hente frem bilder i Frontend
+function hasThumbnail(photo: unknown): photo is Bookcover & GuardTypes {
+  if (!photo || typeof photo !== 'object' || !('sizes' in photo)) {
+    return false
+  }
+
+  const sizes = (photo as any).sizes
+  const thumbnail = sizes?.thumbnail
+
+  return (
+    thumbnail &&
+    typeof thumbnail.url === 'string' &&
+    typeof thumbnail.width === 'number' &&
+    typeof thumbnail.height === 'number'
+  )
 }
 
 export default async function BooksPage({ params }: BooksPageProps) {
@@ -27,8 +56,14 @@ export default async function BooksPage({ params }: BooksPageProps) {
     <main>
       <h1>BookDragons bøker</h1>
       <section>
-        {books.map((book) => (
-          <div key={book.id}>
+        {books.map((book) => {
+          if (!hasThumbnail(book.cover)) {
+            return null
+          }
+
+          return(
+
+            <div key={book.id}>
             <Link href={`${goToPage}/bok/${book.slug}`} key={book.id}>
               {/* Legg inn typeguard på author, sjanger og omslag siden det kommer som objekt fra
             Payload */}
@@ -38,15 +73,16 @@ export default async function BooksPage({ params }: BooksPageProps) {
                 cover={book.cover.sizes.thumbnail.url}
                 author={book.author.name}
                 genre={book.genre.name}
-              ></BookCard>
+                ></BookCard>
             </Link>
             <Button
               type="button"
               variant={book.stock <= 0 ? 'disabled' : 'primary'}
               text={book.stock <= 0 ? 'Ikke på lager' : 'Legg i handlekurv'}
-            ></Button>
+              ></Button>
           </div>
-        ))}
+            )
+        })}
       </section>
 
       {/* Framgangsmåte for paginering er hentet fra MinGA under Julesanger med paginering/Redirect og URL-parametre: 
