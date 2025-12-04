437ae162db464320654c42c6f925bb7725d60f7f
Silje Skogstrand Moe <siljeskogstrand@gmail.com>
2025-12-04 12:28:37 +0100

    feat: side for bestillinger vise oppsummering av bestilling

--- src/app/(frontend)/(without-cart)/bestilling/page.tsx
+++ src/app/(frontend)/(without-cart)/bestilling/page.tsx
@@ -1,10 +1,26 @@
+'use client'
+
 import OrderForm from '@/components/OrderForm/OrderForm'
+import { useCartStore } from '@/store/cartStore'
 
 export default function OrderPage() {
+  const cart = useCartStore((state) => state.cart)
+  let total = 0
+
+  cart.forEach((item) => {
+    total += item.price * item.quantity
+  })
   return (
     <section>
       <h1>Legg inn din bestilling</h1>
-      {/* Handlekurvoppsummering? */}
+      {cart.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>Antall: {item.quantity}</p>
+          <p>Sum: {item.price * item.quantity} kr</p>
+        </div>
+      ))}
+      <h3>Til sammen: {total} kr</h3>
       <OrderForm></OrderForm>
     </section>
   )
--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -1,12 +1,16 @@
 import Button from '../Button/Button'
 
 export default function OrderForm() {
+    function handleSubmit(){
+
+    }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <h2>Fyll ut skjemaet for å sende bestillingen</h2>
       <label htmlFor="name">Navn</label>
-      <input type="text" name="name" />
+      <input type="text" name="name" required/>
       <label htmlFor="email">E-post</label>
-      <input type="email" name="email" />
+      <input type="email" name="email" required/>
       <Button type="submit" variant="primary" text="Send inn bestilling" disabled={false}></Button>
     </form>
   )
