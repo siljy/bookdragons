427fbc14317d4e18522bafb34a78a9e9aa7a75d1
Silje Skogstrand Moe <siljeskogstrand@gmail.com>
2025-12-05 16:52:23 +0100

    feat: satt opp side for spesifikk sjanger

---
+++ src/app/(frontend)/(with-cart)/sjanger/[genreSlug]/page.tsx
@@ -0,0 +1,50 @@
+//Viser fram sjanger
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import PresentationArticle from '@/components/PresentationArticle/PresentationArticle'
+import ErrorMessage from '@/components/ErrorMessage/ErrorMessage'
+
+type GenrePageParams = {
+  params: Promise<{ genreSlug: string }>
+}
+
+export default async function GenrePage({ params }: GenrePageParams) {
+  const { genreSlug } = await params
+  const payload = await getPayload({ config })
+
+  const queryResultsGenres = await payload.find({
+    collection: 'genres',
+    where: {
+      slug: {
+        equals: genreSlug,
+      },
+    },
+  })
+
+  const genre = queryResultsGenres.docs[0]
+
+  if (!genre) {
+    return <ErrorMessage message="Denne sjangerens finnes ikke"></ErrorMessage>
+  }
+
+  const queryResultBooks = await payload.find({
+    collection: 'books',
+    where: {
+      author: {
+        equals: genre.id,
+      },
+    },
+  })
+
+  const books = queryResultBooks.docs
+
+  return (
+    <main>
+      <PresentationArticle
+        name={genre.name}
+        presentation={genre.presentation}
+        books={books}
+      ></PresentationArticle>
+    </main>
+  )
+}
