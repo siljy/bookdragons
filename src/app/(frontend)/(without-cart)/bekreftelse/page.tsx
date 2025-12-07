479607e8bcfecb8f231ff46151ebe16d56ef0502
Silje Skogstrand Moe <siljeskogstrand@gmail.com>
2025-12-07 13:09:33 +0100

    style: enkel styling på ordrebekreftelse. brukt flex for å skape litt whitespace, lignende slik på bestillingsside

--- src/app/(frontend)/(without-cart)/bekreftelse/page.tsx
+++ src/app/(frontend)/(without-cart)/bekreftelse/page.tsx
@@ -1,16 +1,29 @@
 import Link from 'next/link'
+import Button from '@/components/Button/Button'
 
 export default function ConfirmationPage() {
   return (
     <section>
       <h1>Hurra! Bestillingen din er sendt til oss</h1>
-      <h2>Du mottar en e-post når den er klar til å hentes</h2>
-      <p>Om du har noen spørsmål, kontakt oss på kundeservice@bookdragons.com</p>
-      <p>Vår adresse: Eventyrstien 12, Østlandet</p>
+      <div className='orderInformation'>
+        <h2>Hva skjer nå?</h2>
+        <ol>
+          <li>Du mottar en e-post når bestillingen er klar til å hentes</li>
+          <li>Du henter bestillingen din hos oss i Eventyrstien 3</li>
+        </ol>
+        <p>Om du har noen spørsmål, kontakt oss på kundeservice@bookdragons.com</p>
+      </div>
 
-      <div>
+      <div className='orderInformation'>
         <h2>Ute etter mer lesestoff?</h2>
-        <Link href={'1'}>Utforsk utvalget vårt</Link>
+        <Link href={'/'}>
+          <Button
+            text="Utforsk utvalget vårt"
+            type="button"
+            variant="primary"
+            disabled={false}
+          ></Button>
+        </Link>
       </div>
     </section>
   )
