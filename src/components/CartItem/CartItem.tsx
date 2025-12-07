c4f6c68e9e436ba3443ec8bf548721258789382f
Silje Skogstrand Moe <siljeskogstrand@gmail.com>
2025-12-07 10:50:18 +0100

    style: stylet knapper i handlekurv

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -4,6 +4,8 @@ import DecreaseButton from '../DecreaseButton/DecreaseButton'
 import IncreaseButton from '../IncreaseButton/IncreaseButton'
 import RemoveFromCartButton from '../RemoveFromCartButton/RemoveFromCartButton'
 
+import { useCartStore } from '@/store/cartStore'
+
 export default function CartItem({ id, title, quantity, price }: BookInCart) {
   return (
     <div key={id} className={styles.cartItem}>
