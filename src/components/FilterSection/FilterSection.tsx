a9fc75664280bed70268c086299ceeeeec264c69
Silje Skogstrand Moe <siljeskogstrand@gmail.com>
2025-12-07 14:12:50 +0100

    fix: filter hadde feil navn og funket dermed ikke, det gjør den nå

--- src/components/FilterSection/FilterSection.tsx
+++ src/components/FilterSection/FilterSection.tsx
@@ -32,14 +32,14 @@ export default function FilterSection({ authors, genres }: FilterSectionProps) {
     <section className={styles.filterSection}>
       <FilterItem
         title="Filtrer etter forfatter"
-        name="forfatter"
+        name="author"
         options={authors}
         value={searchParams.get('author') || 'all'}
         onChange={handleFilterChange}
       ></FilterItem>
       <FilterItem
         title="Filtrer etter sjanger"
-        name="sjanger"
+        name="genre"
         options={genres}
         value={searchParams.get('genre') || 'all'}
         onChange={handleFilterChange}
